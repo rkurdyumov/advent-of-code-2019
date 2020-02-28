9cd7b8df0a672644ce767253bad13235f2ed4e53
Ruslan Kurdyumov <ruslan88@gmail.com>
2020-02-28 12:48:32 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,1 @@
+Python solutions to Advent of Code 2019 problems.
---
+++ day01.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+import unittest
+
+def get_fuel(mass):
+    return mass//3 - 2
+
+def get_fuel2(mass):
+    fuel = 0
+    while mass > 0:
+        fuel_needed = max(0, get_fuel(mass))
+        fuel += fuel_needed
+        mass = fuel_needed
+    return fuel
+
+class TestPart1(unittest.TestCase):
+    def test_get_fuel(self):
+        self.assertEqual(get_fuel(12), 2)
+        self.assertEqual(get_fuel(14), 2)
+        self.assertEqual(get_fuel(1969), 654)
+        self.assertEqual(get_fuel(100756), 33583)
+
+class TestPart2(unittest.TestCase):
+    def test_get_fuel2(self):
+        self.assertEqual(get_fuel2(14), 2)
+        self.assertEqual(get_fuel2(1969), 966)
+        self.assertEqual(get_fuel2(100756), 50346)
+
+def main():
+    # Run from command line with "python3 -m unittest day01.py".
+    # unittest.main()
+
+    total_fuel = 0
+    total_fuel2 = 0
+    with open("day01.txt") as input_file:
+        for line in input_file:
+            total_fuel += get_fuel(int(line))
+            total_fuel2 += get_fuel2(int(line))
+    print(total_fuel)
+    print(total_fuel2)
+
+if __name__ == '__main__':
+    main()
