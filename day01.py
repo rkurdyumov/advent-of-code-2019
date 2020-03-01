9742b9e315257f2fa13d3f3900f6f1706055b66b
Ruslan Kurdyumov <ruslan88@gmail.com>
2020-02-29 17:24:29 -0800

    Added day2 solution.

--- day01.py
+++ day01.py
@@ -13,31 +13,28 @@ def get_fuel2(mass):
         mass = fuel_needed
     return fuel
 
-class TestPart1(unittest.TestCase):
-    def test_get_fuel(self):
-        self.assertEqual(get_fuel(12), 2)
-        self.assertEqual(get_fuel(14), 2)
-        self.assertEqual(get_fuel(1969), 654)
-        self.assertEqual(get_fuel(100756), 33583)
-
-class TestPart2(unittest.TestCase):
-    def test_get_fuel2(self):
-        self.assertEqual(get_fuel2(14), 2)
-        self.assertEqual(get_fuel2(1969), 966)
-        self.assertEqual(get_fuel2(100756), 50346)
-
 def main():
-    # Run from command line with "python3 -m unittest day01.py".
-    # unittest.main()
-
     total_fuel = 0
     total_fuel2 = 0
     with open("day01.txt") as input_file:
         for line in input_file:
             total_fuel += get_fuel(int(line))
             total_fuel2 += get_fuel2(int(line))
-    print(total_fuel)
-    print(total_fuel2)
+    print("Part 1 fuel required: {}".format(total_fuel))
+    print("Part 2 fuel required: {}".format(total_fuel2))
 
 if __name__ == '__main__':
     main()
+
+class TestGetFuel(unittest.TestCase):
+    def test_get_fuel(self):
+        self.assertEqual(get_fuel(12), 2)
+        self.assertEqual(get_fuel(14), 2)
+        self.assertEqual(get_fuel(1969), 654)
+        self.assertEqual(get_fuel(100756), 33583)
+
+    def test_get_fuel2(self):
+        self.assertEqual(get_fuel2(14), 2)
+        self.assertEqual(get_fuel2(1969), 966)
+        self.assertEqual(get_fuel2(100756), 50346)
+
---
+++ day02.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+from itertools import product
+import unittest
+
+OPCODE_ADD = 1
+OPCODE_MULTIPLY = 2
+OPCODE_HALT = 99
+
+def run_intcode_program(ints, noun=None, verb=None):
+    ints[1] = ints[1] if noun is None else noun
+    ints[2] = ints[2] if verb is None else verb
+    i = 0
+    while ints[i] != OPCODE_HALT:
+        if ints[i] == 1:
+            ints[ints[i+3]] = ints[ints[i+1]] + ints[ints[i+2]]
+        elif ints[i] == 2:
+            ints[ints[i+3]] = ints[ints[i+1]] * ints[ints[i+2]]
+        else:
+            raise ValueError("{} has invalid opcode {} at position {}.".format(
+                             ints[0:i], ints[i], i))
+        i+=4
+    return ints
+
+def main():
+    with open("day02.txt") as input_file:
+        ints = [int(x) for x in input_file.read().split(",")]
+
+    run_ints = run_intcode_program(ints.copy(), noun=12, verb=2)
+    print("Part 1 output: {}".format(run_ints[0]))
+
+    for noun, verb in product(range(100), repeat=2):
+        run_ints = run_intcode_program(ints.copy(), noun, verb)
+        if run_ints[0] == 19690720:
+            print("Part 2 output: {} for verb={},noun={}".format(
+                  100 * noun + verb, verb, noun))
+            break
+
+if __name__ == '__main__':
+    main()
+
+class TestRunIntcodeProgram(unittest.TestCase):
+    def test_run_intcode_program(self):
+        self.assertEqual(run_intcode_program([1,0,0,0,99]), [2,0,0,0,99])
+        self.assertEqual(run_intcode_program([2,3,0,3,99]), [2,3,0,6,99])
+        self.assertEqual(run_intcode_program([2,4,4,5,99,0]),
+                                             [2,4,4,5,99,9801])
+        self.assertEqual(run_intcode_program([1,1,1,4,99,5,6,0,99]),
+                                             [30,1,1,4,2,5,6,0,99])
+        self.assertEqual(run_intcode_program([1,9,10,3,2,3,11,0,99,30,40,50]),
+                                             [3500,9,10,70,2,3,11,0,99,30,40,50])
+        self.assertRaises(ValueError, run_intcode_program, [3,0,0,0,99])
