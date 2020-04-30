3954211ef1d868328f337c7e7dbc436c9604b206
Ruslan Kurdyumov <ruslan88@gmail.com>
2020-04-29 19:18:30 -0700

    Clean up day 6.

--- day06.py
+++ day06.py
@@ -1,158 +1,137 @@
-from collections import defaultdict
 import unittest
 
-class Tree:
-    def __init__(self, map_data):
-        self.nodes = set()
-        self.children = defaultdict(list)
-        self.parents = {}
-        entries = [entry.split(")") for entry in map_data.split()]
-        for parent, child in entries:
-            self.nodes.add(parent)
-            self.nodes.add(child)
-            self.children[parent].append(child)
-            if child in self.parents:
-                raise ValueError("Node {} has parent {}. Cannot add {}".format(
-                                 child, self.parents[child], parent))
-            self.parents[child] = parent
+class OrbitMap:
+    def __init__(self, orbits):
+        self.children = set()
+        self.parents = {} # {satellite: primary}
+        for primary, satellite in orbits:
+            self.children.add(satellite)
+            if satellite in self.parents:
+                raise ValueError("Object {} is orbiting {}. Cannot orbit {}."
+                    .format(satellite, self.parents[satellite], primary))
+            self.parents[satellite] = primary
+        root = self._get_ancestors(next(iter(self.children)))[-1]
+        for c in self.children:
+            if root != self._get_ancestors(c)[-1]:
+                raise ValueError("Invalid orbit map: multiple COM: {}, {}."
+                                 .format(root, self._get_ancestors(c)[-1]))
 
-    def get_root(self):
-        if not self.is_valid():
-            return None
-        all_children = sum(self.children.values(), [])
-        root = set(self.nodes) - set(all_children)
-        assert(len(root) == 1)
-        return root.pop()
-
-    def is_valid(self):
-        all_children = sum(self.children.values(), [])
-        # This only works if there are no loops in the tree.
-        return len(self.nodes) == len(all_children) + 1
+    def _get_ancestors(self, obj):
+        ancestors = [obj]
+        visited = set()
+        while ancestors[-1] in self.parents:
+            if ancestors[-1] in visited:
+                raise ValueError("Cycle in orbit map: {}.". format(ancestors))
+            visited.add(ancestors[-1])
+            ancestors.append(self.parents[ancestors[-1]])
+        return ancestors[1:]
 
     def count_orbits(self):
-        all_children = sum(self.children.values(), [])
         num_orbits = 0
-        for child in all_children:
-            while child in self.parents:
-                num_orbits += 1
-                child = self.parents[child]
+        # This could be improved by caching num_orbits for each ancestor.
+        for child in self.children:
+            num_orbits += len(self._get_ancestors(child))
         return num_orbits
     
     def min_transfers(self, a, b):
-        if a == b:
-            return 0
-        if self.get_root() in {a, b}:
-            return -1  # Root doesn't orbit anything.
-        
-        a_parents = []
-        a_parent = a
-        while a_parent in self.parents:
-             a_parents.append(self.parents[a_parent])
-             a_parent = self.parents[a_parent]
-
-        b_parents = []
-        b_parent = b
-        while b_parent in self.parents:
-             b_parents.append(self.parents[b_parent])
-             b_parent = self.parents[b_parent]
-        
-        if a in b_parents:
-            return b_parents.index(a) + 1
-        if b in a_parents:
-            return a_parents.index(b) + 1
- 
-        common_ancestors = [p for p in b_parents if p in a_parents]
-        closest_ancestor = common_ancestors[0]
-        return a_parents.index(closest_ancestor) + b_parents.index(
-            closest_ancestor)
+        a_ancestors = self._get_ancestors(a)
+        b_ancestors = self._get_ancestors(b)
+        for i, ancestor in enumerate(a_ancestors):
+            if ancestor in b_ancestors:
+                return i + b_ancestors.index(ancestor)
+        return -1
 
 def main():
     with open("day06.txt") as input_file:
-        map_data = input_file.read()
-    orbits = Tree(map_data)
-
-    print("Part 1 num_orbits: {}".format(orbits.count_orbits()))
-    print("Part 2 min_transfers: {}".format(orbits.min_transfers("YOU", "SAN")))
+        orbits = [entry.split(")") for entry in input_file.read().split()]
+    orbit_map = OrbitMap(orbits)
+    print("Part 1 orbits: {}".format(orbit_map.count_orbits()))
+    print("Part 2 transfers: {}".format(orbit_map.min_transfers("YOU", "SAN")))
 
 if __name__ == '__main__':
     main()
 
-        
-class TestOrbits(unittest.TestCase):
-    def test_valid_map(self):
-        valid_map = """
-        COM)B
-        B)C
-        COM)D
-        """
-        self.assertTrue(Tree(valid_map).is_valid())
+class TestOrbitMap(unittest.TestCase):
 
-        multiple_root_map = """
-        COM)B
-        A)C
-        """
-        self.assertFalse(Tree(multiple_root_map).is_valid())
+    def test_invalid_maps(self):
+        multiple_parent_map = [
+            ["COM", "B"],
+            ["A", "B"]]
+        self.assertRaises(ValueError, OrbitMap, multiple_parent_map)
 
-        multiple_parent_map = """
-        COM)B
-        A)B
-        """
-        self.assertRaises(ValueError, Tree, multiple_parent_map)
+        multiple_root_map = [
+            ["COM", "B"],
+            ["A", "C"]]
+        self.assertRaises(ValueError, OrbitMap, multiple_root_map)
 
-        cycle_map = """
-        COM)B
-        B)C
-        C)COM
-        """
-        self.assertFalse(Tree(cycle_map).is_valid())
+        cycle_map = [
+            ["COM", "B"],
+            ["B", "C"],
+            ["C", "COM"]]
+        self.assertRaises(ValueError, OrbitMap, cycle_map)
 
-        cycle_map_with_multiple_roots = """
-        COM)B
-        C)C
-        C)COM
-        A)D
-        """
-        # TODO: This should fail!
-        # self.assertFalse(Tree(cycle_map).is_valid())
+        cycle_map_with_multiple_roots = [
+            ["COM", "B"],
+            ["C", "C"],
+            ["C", "COM"],
+            ["A", "D"]]
+        self.assertRaises(ValueError, OrbitMap, cycle_map_with_multiple_roots)
 
     def test_count_orbits(self):
-        simple_map = """
-        COM)B
-        B)C
-        COM)D
-        """
-        self.assertEqual(Tree(simple_map).count_orbits(), 4)
+        self.assertEqual(OrbitMap([["COM", "B"]]).count_orbits(), 1)
 
-        example_map = """
-        COM)B
-        B)C
-        C)D
-        D)E
-        E)F
-        B)G
-        G)H
-        D)I
-        E)J
-        J)K
-        K)L
-        """
-        self.assertEqual(Tree(example_map).count_orbits(), 42)
+        simple_map = [
+            ["COM", "B"],
+            ["B", "C"],
+            ["COM", "D"]]
+        self.assertEqual(OrbitMap(simple_map).count_orbits(), 4)
+
+        example_map = [
+            ["COM", "B"],
+            ["B", "C"],
+            ["C", "D"],
+            ["D", "E"],
+            ["E", "F"],
+            ["B", "G"],
+            ["G", "H"],
+            ["D", "I"],
+            ["E", "J"],
+            ["J", "K"],
+            ["K", "L"]]
+        self.assertEqual(OrbitMap(example_map).count_orbits(), 42)
 
     def test_min_transfers(self):
-        simple_map = """
-        COM)B
-        B)C
-        C)D
-        COM)E
-        """
-        simple_tree = Tree(simple_map)
-        self.assertEqual(simple_tree.min_transfers("COM","B"), -1)
-        self.assertEqual(simple_tree.min_transfers("COM","C"), -1)
-        self.assertEqual(simple_tree.min_transfers("COM","D"), -1)
-        self.assertEqual(simple_tree.min_transfers("COM","E"), -1)
-        self.assertEqual(simple_tree.min_transfers("B","C"), 1)
-        self.assertEqual(simple_tree.min_transfers("B","D"), 2)
-        self.assertEqual(simple_tree.min_transfers("B","E"), 0)
-        self.assertEqual(simple_tree.min_transfers("C","E"), 1)
-        self.assertEqual(simple_tree.min_transfers("D","E"), 2)
+        simple_map = [
+            ["COM", "B"],
+            ["B", "C"],
+            ["C", "D"],
+            ["COM", "E"]]
+        simple_map = OrbitMap(simple_map)
+        self.assertEqual(simple_map.min_transfers("COM","B"), -1)
+        self.assertEqual(simple_map.min_transfers("COM","C"), -1)
+        self.assertEqual(simple_map.min_transfers("COM","D"), -1)
+        self.assertEqual(simple_map.min_transfers("COM","E"), -1)
+        self.assertEqual(simple_map.min_transfers("B","B"), 0)
+        self.assertEqual(simple_map.min_transfers("B","C"), 1)
+        self.assertEqual(simple_map.min_transfers("B","D"), 2)
+        self.assertEqual(simple_map.min_transfers("B","E"), 0)
+        self.assertEqual(simple_map.min_transfers("C","E"), 1)
+        self.assertEqual(simple_map.min_transfers("D","E"), 2)
+
+        example_map = [
+            ["COM", "B"],
+            ["B", "C"],
+            ["C", "D"],
+            ["D", "E"],
+            ["E", "F"],
+            ["B", "G"],
+            ["G", "H"],
+            ["D", "I"],
+            ["E", "J"],
+            ["J", "K"],
+            ["K", "L"],
+            ["K", "YOU"],
+            ["I", "SAN"]]
+        example_map = OrbitMap(example_map)
+        self.assertEqual(example_map.min_transfers("YOU","SAN"), 4)
         
